e1761bbccf9a1da8ecfe0d8600ea1b0a011a2e02
Irfan Sayyed <irfan@saaragh.in>
2024-01-08 13:10:37 +0530

    Inital Commit

---
+++ codegen-ui/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div>
+                Vite-ShadCn-Template
+            </div>
+        </>
+    );
+}
+
+export default App;
